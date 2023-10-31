bdc04020ee9a8af2d4f7b2641d23724dada44e5e
OscarSuaz <omanuel_suaznavar@hotmail.com>
2023-10-30 18:37:59 -0600

    [Update] Correción en creación de tablas

--- database/migrations/2023_10_31_000540_add__admin_to__user.php
+++ database/migrations/2023_10_31_000540_add__admin_to__user.php
@@ -11,6 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('admin')->default(0);
         });
