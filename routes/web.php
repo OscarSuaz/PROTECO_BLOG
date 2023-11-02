b5111161d587c034ad24c7d8c9d833a4097fb1a6
David Julian Muñoz Norberto <143279636+davidjulian7@users.noreply.github.com>
2023-11-02 14:34:05 -0600

    cree boton para lelvar a comentarios

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::resource('/comments', CommentController::class)->names([
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
